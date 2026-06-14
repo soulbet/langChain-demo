0a871f0e644440cd420f7c3874c90edf8a1c814c
zyf <zyf935612@gmail.com>
2026-06-14 21:00:00 +0800

    20260614

--- main/module/chat_module/demo01-bind_tools/demo.py
+++ main/module/chat_module/demo01-bind_tools/demo.py
@@ -1,4 +1,7 @@
+from langchain_core.output_parsers import PydanticToolsParser
 from pydantic import BaseModel, Field
+
+from main.model_factory import model_factory
 
 
 def add(a: int, b: int) -> int:
@@ -31,8 +34,14 @@ class multiply(BaseModel):
 
     a: int = Field(..., description="First integer")
     b: int = Field(..., description="Second integer")
+tools = [add, multiply]
+llm = model_factory().create_model()
 llm_with_tools = llm.bind_tools(tools)
 
+
 query = "What is 3 * 12?"
+
+chain = llm_with_tools | PydanticToolsParser(tools=[add, multiply])
+chain.invoke(query)
 
 llm_with_tools.invoke(query)
---
+++ main/module/chat_module/demo02-structured_output_data/more_select_formatted.py
@@ -0,0 +1,34 @@
+# Pydantic
+from typing import Optional, Union
+
+from pydantic import BaseModel, Field
+
+from main.model_factory import model_factory
+
+
+class Joke(BaseModel):
+    """Joke to tell user."""
+
+    setup: str = Field(description="The setup of the joke")
+    punchline: str = Field(description="The punchline to the joke")
+    rating: Optional[int] = Field(
+        default=None, description="How funny the joke is, from 1 to 10"
+    )
+
+
+class ConversationalResponse(BaseModel):
+    """Respond in a conversational manner. Be kind and helpful."""
+
+    response: str = Field(description="A conversational response to the user's query")
+
+
+class FinalResponse(BaseModel):
+    final_output: Union[Joke, ConversationalResponse]
+
+llm=model_factory().create_model()
+structured_llm = llm.with_structured_output(FinalResponse)
+
+# 自动选择哪个模式进行格式化
+structured_llm.invoke("Tell me a joke about cats")
+
+structured_llm.invoke("How are you today?")
