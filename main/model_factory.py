6cf83184481559a479e12704360a7a1c763bbc39
zyf <zyf935612@gmail.com>
2026-07-01 17:29:19 +0800

    20260701

--- main/model_factory.py
+++ main/model_factory.py
@@ -13,9 +13,9 @@ class model_factory:
         :return:
         """
         self.model = ChatOpenAI(
-            model="deepseek-chat",  # DeepSeek 模型名称
-            openai_api_key="sk-83d73e8bca6948159a4f2d2b6d9abf94",  # 👈 DeepSeek 的 API Key
-            openai_api_base="https://api.deepseek.com/v1",  # 👈 DeepSeek 的 API 端点
+            model="qwen3.7-plus",  # DeepSeek 模型名称
+            openai_api_key="sk-06aa98d2df7d4b0e9b7b540df2b7c37a",  # 👈 DeepSeek 的 API Key
+            openai_api_base="https://ws-m29q37nbk63foudn.cn-beijing.maas.aliyuncs.com/compatible-mode/v1",  # 👈 DeepSeek 的 API 端点
             temperature=0.7,
             max_tokens=1024,
             logprobs=True,  # 开启 logprobs
