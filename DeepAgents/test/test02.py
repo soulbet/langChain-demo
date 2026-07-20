ba74c291389e3e2cf44fbd59fe19d7abfe8c7603
zyf <zyf935612@gmail.com>
2026-07-20 17:45:42 +0800

    20260720

--- DeepAgents/test/test02.py
+++ DeepAgents/test/test02.py
@@ -1,5 +1,5 @@
 from model_factory.model_factory import ModelFactory
 
-llm = ModelFactory().create_model(local_model_type="agent")
+llm = ModelFactory().create_model("qwen")
 result= llm.invoke("请使用纯文本公式，写出 Yeo-Johnson 变换的公式。")
-print(result.content)
+print(f"result:{result.content}")
